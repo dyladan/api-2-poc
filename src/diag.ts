6aaef219adc4cff2fd7e0b2c7b1d357753cf36cc
Daniel Dyla <dyladan@users.noreply.github.com>
2025-06-07 00:21:24 -0400

    Short circuit diag if no listeners

--- src/diag.ts
+++ src/diag.ts
@@ -3,21 +3,26 @@ import { channel } from "./channels";
 const ch = channel("@opentelemetry/api/diag");
 
 export function error(message: string, body?: any) {
+  if (!ch.hasSubscribers) return;
   ch.publish({ level: "error", message, body });
 }
 
 export function warn(message: string, body?: any) {
+  if (!ch.hasSubscribers) return;
   ch.publish({ level: "warn", message, body });
 }
 
 export function info(message: string, body?: any) {
+  if (!ch.hasSubscribers) return;
   ch.publish({ level: "info", message, body });
 }
 
 export function debug(message: string, body?: any) {
+  if (!ch.hasSubscribers) return;
   ch.publish({ level: "debug", message, body });
 }
 
 export function trace(message: string, body?: any) {
+  if (!ch.hasSubscribers) return;
   ch.publish({ level: "trace", message, body });
 }
